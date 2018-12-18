b7c79a013485b52f897a85ddd988dac034316c98
Voltis <v.agolli@live.com>
2018-12-18 21:50:51 +0100

    swipe out animation

--- src/Deck.js
+++ src/Deck.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
     constructor(props) {
@@ -13,12 +15,26 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({ x: gesture.dx, y: gesture.dy });
             },
-            onPanResponderRelease: () => {
-                this.resetPosition();
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
             }
         });
 
         this.state = { panResponder, position };
+    }
+
+    forceSwipe(direction) {
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+            toValue: { x: x, y: 0 },
+            duration: SWIPE_OUT_DURATION
+        }).start();
     }
 
     resetPosition() {
