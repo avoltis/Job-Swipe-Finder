7bff7217c79f0b9f738520c05990695e277aa990
Voltis <v.agolli@live.com>
2018-12-18 22:33:46 +0100

    no more card case

--- App.js
+++ App.js
@@ -29,11 +29,22 @@ export default class App extends React.Component {
 
   }
 
+  renderNoMoreCards() {
+    return (
+      <Card title="All Done">
+        <Text style={{ marginBottom: 10 }}>
+          There's no more content here!
+        </Text>
+        <Button title="Get More!" backgroundColor="#03A9F4" />
+      </Card>
+    );
+  }
+
 
   render() {
     return (
       <View style={styles.container}>
-        <Deck data={DATA} renderCard={this.renderCard} />
+        <Deck data={DATA} renderCard={this.renderCard} renderNoMoreCards={this.renderNoMoreCards} />
       </View>
     );
   }
