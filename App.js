517d3399c17de956d7c36d3a9dd4eb2bbbed099c
Voltis <v.agolli@live.com>
2018-12-20 00:44:24 +0100

    sign in form

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm'
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    const config = {
+      //firebase config data
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -17,6 +27,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
