704f92fb2cd44c4f32a43fafe5f3f0b788c1f162
Voltis <v.agolli@live.com>
2018-12-20 13:08:40 +0100

    header style

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Platform } from 'react-native';
 import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
@@ -7,8 +7,17 @@ class ReviewScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
         headerTitle: 'Review Jobs',
         headerRight: (
-            <Button title="Settings" onPress={() => navigation.navigate('settings')} />
-        )
+            <Button
+                title="Settings"
+                onPress={() => navigation.navigate('settings')}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                color="rgba(0, 122, 255, 1)"
+                
+            />
+        ),
+        headerStyle: {
+            marginTop: Platform.OS === "android" ? 24 : 0
+          }
     })
 
     render() {
