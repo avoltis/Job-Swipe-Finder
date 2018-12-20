d07e47b4ce13ee32380f08d9596ef89930f1a073
Voltis <v.agolli@live.com>
2018-12-20 12:00:42 +0100

    main app sceleteon navigation

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from "react-navigation"
+import { createBottomTabNavigator, createAppContainer, createStackNavigator } from "react-navigation"
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeskScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import Settings from './screens/SettingsScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 export default class App extends React.Component {
 
   render() {
     const MainNavigator = createBottomTabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: createBottomTabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeskScreen },
+          review: {
+            screen: createStackNavigator({
+              review: ReviewScreen,
+              settings: SettingsScreen
+            })
+          }
+        })
+      }
     });
 
     const App = createAppContainer(MainNavigator);
